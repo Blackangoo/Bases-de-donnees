b243bea8d90f9b2a1ce1d7177215039c4f84fb03
Blackangoo <blackangoo.gc@gmail.com>
2024-04-18 09:18:48 +0200

    added project folder

---
+++ Project/daedalus_structure_bdd.sql
@@ -0,0 +1,14 @@
+-- Suppression de la base de données si elle existe
+DROP DATABASE IF EXISTS swiss_vibes;
+-- Création de la base de données
+CREATE DATABASE swiss_vibes;
+
+-- Utiliser la base de données 'swiss_vibes' pour les requêtes suivantes
+USE swiss_vibes;
+
+-- Suppression et création des tables ainsi que leurs colonnes
+
+DROP TABLE IF EXISTS personne;
+CREATE TABLE personne (
+    
+)
